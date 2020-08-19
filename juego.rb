8b7781a0cad384833927cff0eaa194ae1345dfa5
Melisa Budini <melixbq@gmail.com>
2020-08-19 17:23:16 -0400

    ultimo arreglo del archivo con menos lineas

--- juego.rb
+++ juego.rb
@@ -2,36 +2,19 @@ player = ARGV[0].downcase
 arr = ['piedra', 'papel', 'tijera']
 comp = rand(0..2)
     puts "Computador juega #{arr[comp]}"
-case comp
-when 0
-    if player == 'piedra'
-        puts 'Empataste'
-    elsif player == 'tijera'
-        puts 'Perdiste'
-    elsif player == 'papel'
-        puts 'Ganaste'
-    else
-        puts 'Argumento inválido: Debe ser piedra, papel o tijera.'
-    end
-when 1
-    if player == 'papel'
-        puts 'Empataste'
-    elsif player == 'tijera'
-        puts 'Ganaste'
-    elsif player == 'piedra'
-        puts 'Perdiste'
-    else
-        puts 'Argumento inválido: Debe ser piedra, papel o tijera.'
-    end
-when 2
-    if player == 'tijera'
-        puts 'Empataste'
-    elsif player == 'piedra'
-        puts 'Ganaste'
-    elsif player == 'papel'
-        puts 'Perdiste'
-    else
-        puts 'Argumento inválido: Debe ser piedra, papel o tijera.'
-    end
-
+case player
+when 'piedra'
+    puts 'Empataste' if comp == 0
+    puts 'Perdiste' if comp == 1
+    puts 'Ganaste' if comp == 2
+when 'papel'
+    puts 'Empataste' if comp == 1
+    puts 'Ganaste' if comp == 0
+    puts 'Perdiste' if comp == 2
+when 'tijera'
+    puts 'Empataste' if comp == 2 
+    puts 'Ganaste' if comp == 1
+    puts 'Perdiste' if comp == 0
+else
+    puts 'Argumento inválido: Debe ser piedra, papel o tijera.'
 end
