9d6c8f9931c1a8fe9cbeae66748a723e44322f6d
Melisa Budini <melixbq@gmail.com>
2020-08-19 20:13:42 -0400

    arreglado arg inv sin comp juega

--- juego.rb
+++ juego.rb
@@ -1,20 +1,19 @@
 player = ARGV[0].downcase
 arr = ['piedra', 'papel', 'tijera']
 comp = rand(0..2)
-    puts "Computador juega #{arr[comp]}"
 case player
 when 'piedra'
-    puts 'Empataste' if comp == 0
-    puts 'Perdiste' if comp == 1
-    puts 'Ganaste' if comp == 2
+    puts "Computador juega #{arr[comp]}. \nEmpataste" if comp == 0
+    puts "Computador juega #{arr[comp]}. \nPerdiste" if comp == 1
+    puts "Computador juega #{arr[comp]}. \nGanaste" if comp == 2
 when 'papel'
-    puts 'Empataste' if comp == 1
-    puts 'Ganaste' if comp == 0
-    puts 'Perdiste' if comp == 2
+    puts "Computador juega #{arr[comp]}. \nEmpataste" if comp == 1
+    puts "Computador juega #{arr[comp]}. \nGanaste" if comp == 0
+    puts "Computador juega #{arr[comp]}. \nPerdiste" if comp == 2
 when 'tijera'
-    puts 'Empataste' if comp == 2 
-    puts 'Ganaste' if comp == 1
-    puts 'Perdiste' if comp == 0
+    puts "Computador juega #{arr[comp]}. \nEmpataste" if comp == 2 
+    puts "Computador juega #{arr[comp]}. \nGanaste" if comp == 1
+    puts "Computador juega #{arr[comp]}. \nPerdiste" if comp == 0
 else
     puts 'Argumento inválido: Debe ser piedra, papel o tijera.'
 end
